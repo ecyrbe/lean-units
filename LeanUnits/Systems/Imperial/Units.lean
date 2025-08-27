f935f8d0739644a449f620064a297b08c19cf797
ecyrbe <ecyrbe@gmail.com>
2025-08-28 01:43:08 +0200

    add imperial units

--- LeanUnits/Systems/Imperial/Units.lean
+++ LeanUnits/Systems/Imperial/Units.lean
@@ -7,4 +7,21 @@ namespace Units.Unit
 def fahrenheit := defineDerivedUnit "°F" Unit.kelvin
   ((Conversion.scale (5/9) (by simp)) + Conversion.translate (45967 / 100 * 5/9 ))
 
+-- Length
+def inch := defineDerivedUnit "in" Unit.meter (Conversion.scale (254/10000) (by simp))
+def foot := defineDerivedUnit "ft" inch (Conversion.scale (12) (by simp))
+def yard := defineDerivedUnit "yd" foot (Conversion.scale (3) (by simp))
+def mile := defineDerivedUnit "mi" yard (Conversion.scale (1760) (by simp))
+-- Mass
+def ounce := defineDerivedUnit "oz" Unit.kilogram (Conversion.scale (28349523125 / 10^11) (by simp))
+def pound := defineDerivedUnit "lb" ounce (Conversion.scale (16) (by simp))
+def ton := defineDerivedUnit "ton" pound (Conversion.scale (2000) (by simp))
+def stone := defineDerivedUnit "st" pound (Conversion.scale (14) (by simp))
+-- Volume
+def gallon := defineDerivedUnit "gal" (meter^3) (Conversion.scale (231 * 254^3 / 10^12) (by simp))
+def quart := defineDerivedUnit "qt" gallon (Conversion.scale (1/4) (by simp))
+def pint := defineDerivedUnit "pt" quart (Conversion.scale (1/2) (by simp))
+def cup := defineDerivedUnit "cup" pint (Conversion.scale (1/2) (by simp))
+def fluid_ounce := defineDerivedUnit "fl oz" gallon (Conversion.scale (1/128) (by simp))
+
 end Units.Unit
