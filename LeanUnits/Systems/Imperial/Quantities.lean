797b161d25c022d17319bf079d5ad59936bc5700
ecyrbe <ecyrbe@gmail.com>
2025-08-28 02:21:31 +0200

    feat(imperial): add additional imperial units for length, mass, and volume

--- LeanUnits/Systems/Imperial/Quantities.lean
+++ LeanUnits/Systems/Imperial/Quantities.lean
@@ -8,4 +8,21 @@ abbrev Imperial {μ} (units : μ) := Quantity units Float
 
 @[inline] def F₀ : Imperial Unit.fahrenheit := ⟨1.0⟩ -- 1 degree Fahrenheit
 
+-- length
+@[inline] def inch : Imperial Unit.inch := ⟨1.0⟩ -- 1 inch
+@[inline] def ft : Imperial Unit.foot := ⟨1.0⟩ -- 1 foot
+@[inline] def yd : Imperial Unit.yard := ⟨1.0⟩ -- 1 yard
+@[inline] def mi : Imperial Unit.mile := ⟨1.0⟩ -- 1 mile
+-- mass
+@[inline] def oz : Imperial Unit.ounce := ⟨1.0⟩ -- 1 ounce
+@[inline] def lb : Imperial Unit.pound := ⟨1.0⟩ -- 1 pound
+@[inline] def st : Imperial Unit.stone := ⟨1.0⟩ -- 1 stone
+@[inline] def ton : Imperial Unit.ton := ⟨1.0⟩ -- 1 ton
+-- volume
+@[inline] def gal : Imperial Unit.gallon := ⟨1.0⟩ -- 1 gallon
+@[inline] def qt : Imperial Unit.quart := ⟨1.0⟩ -- 1 quart
+@[inline] def pt : Imperial Unit.pint := ⟨1.0⟩ -- 1 pint
+@[inline] def cup : Imperial Unit.cup := ⟨1.0⟩ -- 1 cup
+@[inline] def fl_oz : Imperial Unit.fluid_ounce := ⟨1.0⟩ -- 1 fluid ounce
+
 end Units
