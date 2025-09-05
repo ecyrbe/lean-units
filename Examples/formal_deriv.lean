41519033eb756b66782d9d9918786a6a020bd6b9
ecyrbe <ecyrbe@gmail.com>
2025-09-05 17:08:34 +0200

    refactor: use quantity type definitions to use WithDim for for analogy with PhysLean

--- Examples/formal_deriv.lean
+++ Examples/formal_deriv.lean
@@ -3,23 +3,23 @@ import LeanUnits.Framework.Quantities.Lemmas
 import Mathlib.Data.Real.Basic
 
 namespace formal
-open Units Real
+open Units Real Dimension
 
-abbrev SI (units : Dimension) := Quantity units ℝ
+abbrev WithDim (units : Dimension) := Quantity units ℝ
 
 noncomputable def speed_from_pos_over_time
-  (t : SI Dimension.Time) (v0 : SI Dimension.Speed) (angle : ℝ) : SI Dimension.Speed :=
+  (t : WithDim Time) (v0 : WithDim Speed) (angle : ℝ) :WithDim Speed :=
   ↑Quantity.deriv (fun t => Real.cos angle • v0 * t) t
 
 -- proof using Quantities lemmas
-theorem simp_over_time (t : SI Dimension.Time) (v0 : SI Dimension.Speed) (angle : ℝ) :
+theorem simp_over_time (t : WithDim Time) (v0 : WithDim Speed) (angle : ℝ) :
     Quantity.deriv (fun t => Real.cos angle • v0 * t) t = ↑(Real.cos angle•v0) := by
     rw [Quantity.deriv_qconst_mul_real,Quantity.deriv_id, Quantity.mul_one]
     · rfl
     · exact Quantity.differentiable_fun_id
 
 -- proof using only mathlib lemmas
-theorem simp_over_time' (t : SI Dimension.Time) (v0 : SI Dimension.Speed) (angle : ℝ) :
+theorem simp_over_time' (t : WithDim Time) (v0 : WithDim Speed) (angle : ℝ) :
     Quantity.deriv (fun t => Real.cos angle • v0 * t) t = ↑(Real.cos angle•v0) := by
   simp [Quantity.deriv, Quantity.cast]
   change deriv (fun t : ℝ => Real.cos angle • v0.val * t) t.val = Real.cos angle * v0.val
