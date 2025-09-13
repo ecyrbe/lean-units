f2d70e084e9ee5a240624d94d52f7140de1a7e39
ecyrbe <ecyrbe@gmail.com>
2025-09-13 17:39:29 +0200

    feat(conversion): add factor lemmas

--- LeanUnits/Framework/Conversions/Lemmas.lean
+++ LeanUnits/Framework/Conversions/Lemmas.lean
@@ -66,5 +66,37 @@ theorem scalable_sub_eq_div {c1 c2 : Conversion} (h1 : Scalable c1) (h2 : Scalab
   simp [sub, div, inv,mul, h1, h2]
   rfl
 
+theorem factor_div {c1 c2 : Conversion} :
+  (c1 / c2).factor = c1.factor / c2.factor := by
+  rw [HDiv.hDiv,instHDiv, Div.div]
+  simp [div, mul, inv]
+  rfl
+
+theorem factor_mul {c1 c2 : Conversion} : (c1 * c2).factor = c1.factor * c2.factor := by rfl
+
+theorem factor_inv {c : Conversion} : (c⁻¹).factor = 1 / c.factor := by rfl
+
+theorem factor_add {c1 c2 : Conversion} : (c1 + c2).factor = c1.factor * c2.factor := by rfl
+
+theorem factor_sub {c1 c2 : Conversion} : (c1 - c2).factor = c1.factor / c2.factor := by rfl
+
+theorem factor_nsmul {c : Conversion} (s : ℕ) : (s • c).factor = c.factor ^ s := by rfl
+
+theorem factor_zsmul {c : Conversion} (s : ℤ) : (s • c).factor = c.factor ^ s := by rfl
+
+theorem scalable_apply {α} [Coe ℚ α] [Field α]
+  (c : Conversion) (x : α) (h : Scalable c) (h_coe_zero : Coe.coe (0 : ℚ) = (0 : α) := by simp) :
+  c.apply x = x * Coe.coe c.factor := by
+  unfold Scalable at h
+  simp only [apply, h, h_coe_zero]
+  field_simp
+
+theorem scalable_convert {α} [Coe ℚ α] [Field α]
+  (c1 c2 : Conversion) (x : α) (h1 : Scalable c1) (h2 : Scalable c2)
+  (h_coe_zero : Coe.coe (0 : ℚ) = (0 : α) := by simp) :
+  convert c1 c2 x = x * Coe.coe (c1.factor/c2.factor) := by
+  have h_offset_zero: (c1 / c2).offset = 0 := by exact scalable_div h1 h2
+  simp only [convert, apply, factor_div, h_offset_zero, h_coe_zero]
+  field_simp
 
 end Units.Conversion
