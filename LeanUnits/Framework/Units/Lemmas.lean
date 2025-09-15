338f8c8dece94d74689af2aafda2c51a9ec3edf4
ecyrbe <ecyrbe@gmail.com>
2025-09-15 23:14:49 +0200

    chore(Units): cleanup

--- LeanUnits/Framework/Units/Lemmas.lean
+++ LeanUnits/Framework/Units/Lemmas.lean
@@ -8,10 +8,6 @@ theorem equiv_symm {u1 u2 : Unit} (h : u1 ≈ u2) : u2 ≈ u1 := Setoid.symm h
 
 theorem equiv_trans {u1 u2 u3 : Unit} (h1 : u1 ≈ u2) (h2 : u2 ≈ u3) : u1 ≈ u3 := Setoid.trans h1 h2
 
-theorem eq_imp_equiv {μ} [Setoid μ] {u1 u2 : μ} (h : u1 = u2) : u1 ≈ u2 := by
-  rw [h]
-  exact Setoid.refl u2
-
 @[simp]
 theorem base_unit_dim_eq_dim (d : Dimension) (s : String) :
   (defineUnit s d).dimension = d := by
